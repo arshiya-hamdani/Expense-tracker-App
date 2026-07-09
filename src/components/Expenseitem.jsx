496a179e4a502c77070e9df122b2cd08c7b87b48
arshiya-hamdani <arshiyahamdani@gmail.com>
2026-07-09 16:49:52 +0530

    Update Expenseitem.jsx

--- src/components/Expenseitem.jsx
+++ src/components/Expenseitem.jsx
@@ -1,12 +1,62 @@
 import React from "react";
-function Expenseitem({ item, onDelete }) {
-    return (
 
-        <div className="expense-item">
-            <span>{item.title}</span>
-            <span>₹{item.amt}</span>
-            <button className="delete-btn" onClick={() => onDelete(item.id)}>X</button>
+function Expenseitem({ item, onDelete }) {
+  const icons = {
+    Food: "🍔",
+    Shopping: "🛍️",
+    Transport: "🚗",
+    Bills: "⚡",
+    Health: "💚",
+    Entertainment: "🎬",
+    Education: "📚",
+  };
+
+  return (
+    <div className="expense-item">
+      <div className="expense-left">
+        <div className="icon-box">
+          {icons[item.category] || "💰"}
         </div>
-    )
+
+        <div className="expense-details">
+          <span className="category-badge">
+            {item.category}
+          </span>
+
+          <p>
+            <strong>Payment:</strong> {item.payment}
+          </p>
+
+          {item.description && (
+            <p>
+              <strong>Description:</strong> {item.description}
+            </p>
+          )}
+
+          {item.notes && (
+            <p>
+              <strong>Notes:</strong> {item.notes}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="expense-right">
+        <div className="date-time">
+          <p>{item.date}</p>
+          {item.time && <span>{item.time}</span>}
+        </div>
+
+        <h2 className="amount">
+          ₹ {item.amt}
+        </h2>
+
+        <button
+          className="delete-btn"
+          onClick={() => onDelete(item.id)}>Delete
+        </button>
+      </div>
+    </div>
+  );
 }
-export default Expenseitem
+export default Expenseitem;
